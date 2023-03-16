2d6416428b486c61d50526bf9b53d689828d8d7a
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-16 16:27:04 -0700

    print using puts v2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 int main(void)
 {
-	    puts("Programming is like building a multilingual puzzle\n");
+	    puts("Programming is like building a multilingual puzzle");
 	        return 0;
 }
