a5a433f2f2b7918a730384eb0812788b38306eea
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-16 16:43:07 -0700

    ver 4.1.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,9 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     **/
-
+ *     */
 int main(void)
 {
 	    puts("\"Programming is like building a multilingual puzzle");
-	        return 0;
+	        return (0);
 }
