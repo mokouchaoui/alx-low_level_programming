144a0ce58f50da9e46e429a71f095f731c9449ab
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-16 16:56:09 -0700

    checked num 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		/* Use puts to print the string without printf */
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
