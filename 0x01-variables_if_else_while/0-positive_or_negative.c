63d6e3d9d36a9ae672616400ba44b13a022414a2
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-17 06:42:29 -0700

    Push Version 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,9 +23,8 @@ int main(void)
 	if (n > 0)
 		printf("is positive\n");
 	else if (n == 0)
-        	printf("is zero\n");
-    	else
-        	printf("is negative\n");
-	
+		printf("is zero\n");
+	else
+		printf("is negative\n");
 	return (0);
 }
