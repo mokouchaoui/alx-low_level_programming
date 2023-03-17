988740568cca179f971bc7b9f4d43dbd564751e7
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-17 07:04:45 -0700

    numbers by de

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar(48);
+    	putchar(49);
+    	putchar(50);
+    	putchar(51);
+    	putchar(52);
+    	putchar(53);
+    	putchar(54);
+    	putchar(55);
+    	putchar(56);
+    	putchar(57);
+    	putchar('\n');
+    	return 0;
+}
