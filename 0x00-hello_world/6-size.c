d0b2144ba514f560671967ec074e90aa846be5cb
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-16 17:06:32 -0700

    print all sizes in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types
+ * on the computer it is compiled and run on
  *
  * Return: Always 0.
  */
