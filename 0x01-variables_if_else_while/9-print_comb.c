dd1b94effe6785a9170329263f5888d61e3c6d2e
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-17 08:06:32 -0700

    cmb2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-	int i = 0;
-
-	for (i = 10; i < 99; i++)
-	{
-		putchar(i+'0');
-		putchar(',');
-	}
-
-	putchar('\n');
-	return (0);
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0');
+        if (i < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+    return (0);
 }
