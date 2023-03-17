d63bf55ca9588840d041b5f0f4b493e624038699
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-17 07:10:33 -0700

    numbers by de

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	putchar("0");
-    	putchar("1");
-    	putchar("2");
-    	putchar("3");
-    	putchar("4");
-    	putchar("5");
-    	putchar("6");
-    	putchar("7");
-    	putchar("8");
-    	putchar("9");
+	putchar('0');
+    	putchar('1');
+    	putchar('2');
+    	putchar('3');
+    	putchar('4');
+    	putchar('5');
+    	putchar('6');
+    	putchar('7');
+    	putchar('8');
+    	putchar('9');
     	putchar('\n');
     	return 0;
 }
