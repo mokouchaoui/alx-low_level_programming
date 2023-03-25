25e0c138c84df0ff4ae5ac1a9c0e14ef07c07653
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-25 13:09:33 -0700

    push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,13 @@
  *  * main - This prints _putchar, followed by a new line
  *   *
  *    * Return: The value 0
- *     
 */
 
 int main(void)
 {
 	char my_word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i;
-	
+
 	for (i = 0; i < 8; ++i)
 	{
 		_putchar(my_word[i]);
