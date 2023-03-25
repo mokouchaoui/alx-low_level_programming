af851027aaf4db230dd8e17aa2c559f9d9f6cec7
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-25 13:17:18 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		++c;
+	}
+	_putchar('\n');
+}
