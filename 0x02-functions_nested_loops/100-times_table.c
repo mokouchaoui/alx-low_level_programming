62e4719dfda34bb7631d8760d04913a41ed029d1
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-03-25 13:36:55 -0700

    push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,59 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - prints n times table
- * Description: starting from 0
- * @n: funtion parameter
- * Return: void
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
-
 void print_times_table(int n)
 {
-	int i, j;
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; ++i)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
-			for (j = 1; j <= n; ++j)
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				mystyle(i * j);
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * mystyle - used to reduce the lines of the code
- * @n: parameter to work with
- * Return: void
- */
-void mystyle(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10  + '0');
-		_putchar(n % 10 + '0');
 	}
 }
