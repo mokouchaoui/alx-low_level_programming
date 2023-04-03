10ddb5724bdd9f3f3c5bca818c0e4e1bcbb96020
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-04-03 16:39:55 -0700

    push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _memset - fill memory .
+* @s: pointer
+* @b: hexadecimal number
+* @n: n bytes
+* Return: pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *u = (unsigned char *) s;
+
+	while (n-- > 0x00)
+	{
+		*u++ = b;
+	}
+	return (s);
+}
+
