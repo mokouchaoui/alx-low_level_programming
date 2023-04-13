5b7d7474b182a1a3f93a2537ec530f4f440a3a8b
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-04-12 22:00:01 -0700

    First Push For The mandatory Part

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "alx.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *g = malloc(b);
+
+	if (g == NULL)
+	{
+		exit(98);
+	}
+	return (g);
+}
