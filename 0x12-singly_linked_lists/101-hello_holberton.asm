dcafb424f98e89c036364c5fef6b3574aa5bd691
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-04-28 04:02:33 -0700

    Push

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,19 @@
-section .text
-	global main
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
+
+    section .text
+    global main
+    extern printf
 
 main:
-	mov	edx, len
-	mov	ecx, msg	;message variable
-	mov 	ebx, 1		;write to standard output
-	mov	eax, 4
-	int	0x80		;call kernel
+    push rbp
+    mov rbp, rsp
 
-	mov	eax, 1 		;system call (sys_exit)
-	int	0x80		;call kernel
+    mov rsi, hello
+    mov rdi, format
+    call printf
 
-section .data
-msg db 'Hello, Coding', 0xa	;message to print
-len equ $ - msg			;length of the string
+    pop rbp
+    mov rax, 0
+    ret
