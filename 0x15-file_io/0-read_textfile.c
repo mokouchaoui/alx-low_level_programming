0b0df9ec099719a692fbd393dd6898d9e15e687f
Mohamed Kouchaoui <mokoucha@student.42heilbronn.de>
2023-05-08 12:50:32 +0200

    push ex 00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,41 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t n_read, n_wrote;
-    char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return (0);
+	fd = open(filename, O_RDONLY);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	if (fd == -1)
+		return (0);
 
-    n_read = read(fd, buffer, letters);
-    if (n_read < 0)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-    n_wrote = write(STDOUT_FILENO, buffer, n_read);
-    if (n_wrote < 0)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-    free(buffer);
-    close(fd);
-    return (n_read);
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
