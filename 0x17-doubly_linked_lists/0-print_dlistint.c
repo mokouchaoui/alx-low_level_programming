450d6e0a30d97be88a5f62fe3aaf97b6bb2cfbdb
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-09 10:31:31 +0100

    Refactored code to improve performance and readability.

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h - pointer to head of list to print
+ *
+ * Return: returns the number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n ", h->n);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# alx doubly linked List
