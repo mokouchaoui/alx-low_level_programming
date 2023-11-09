030590d7b16136d7a2933dbe5d1318761a6948d3
Mohamed Kouchaoui <mohamed.kouchaoui19@gmail.com>
2023-11-09 10:34:38 +0100

    Refactor code to improve performance and readability.

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dlistint_s - doubly linked list node
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node struct for alx project
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
